f627da6a293cf4bab31adcbd76426bcd977f2c58
Ben Limo <bensokiplimo@gmail.com>
2022-07-06 13:19:42 +0300

    add spring data jpa

---
+++ src/main/java/com/teckmils/warehousemanagementsystem/data/product/model/Product.java
@@ -0,0 +1,82 @@
+package com.teckmils.warehousemanagementsystem.data.product;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "item_name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Float price;
+
+    @Column(name = "created_at", nullable = false)
+    private BigInteger createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private BigInteger updatedAt;
+
+    public Product(String name, Float price, BigInteger createdAt, BigInteger updatedAt) {
+        this.name = name;
+        this.price = price;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public BigInteger getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(BigInteger createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public BigInteger getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(BigInteger updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
