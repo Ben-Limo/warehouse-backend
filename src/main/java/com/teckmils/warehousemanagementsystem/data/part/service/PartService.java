8be866fac2a22dc2d01e033766ed2f2e634f94b9
Ben Limo <bensokiplimo@gmail.com>
2022-07-07 15:59:29 +0300

    add parts service

--- src/main/java/com/teckmils/warehousemanagementsystem/data/part/service/PartService.java
+++ src/main/java/com/teckmils/warehousemanagementsystem/data/part/service/PartService.java
@@ -1,2 +1,24 @@
-package com.teckmils.warehousemanagementsystem.data.part.service;public class PartService {
+package com.teckmils.warehousemanagementsystem.data.part.service;
+
+import com.teckmils.warehousemanagementsystem.data.part.model.Part;
+import com.teckmils.warehousemanagementsystem.data.part.repository.PartRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PartService {
+    private final PartRepository partRepository;
+
+    public PartService(PartRepository partRepository) {
+        this.partRepository = partRepository;
+    }
+
+    public List<Part> getParts() {
+        Iterable<Part> parts = this.partRepository.findAll();
+        List<Part> partList = new ArrayList<>();
+        parts.forEach(part -> {partList.add(part);});
+        return  partList;
+    }
 }
