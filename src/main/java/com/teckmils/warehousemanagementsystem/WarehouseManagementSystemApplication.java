8b49ba4650c977c762b95d8539f3cc6be665424b
Ben Limo <bensokiplimo@gmail.com>
2022-07-19 17:57:15 +0300

    add root level logging

--- src/main/java/com/teckmils/warehousemanagementsystem/WarehouseManagementSystemApplication.java
+++ src/main/java/com/teckmils/warehousemanagementsystem/WarehouseManagementSystemApplication.java
@@ -1,13 +1,17 @@
 package com.teckmils.warehousemanagementsystem;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WarehouseManagementSystemApplication {
-
+	private static final Logger logger =
+			LoggerFactory.getLogger(WarehouseManagementSystemApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(WarehouseManagementSystemApplication.class, args);
+		logger.info("Application Started");
 	}
 
 }
